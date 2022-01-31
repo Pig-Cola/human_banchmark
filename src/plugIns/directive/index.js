03b7a0ec5e6a4178ea184ba96001bd0907adb420
DaeSeong_Hwang <co.la@kakao.com>
2022-01-31 23:58:08 +0900

    create plugins
    filter 및 directive 등의 위치를
    plugins생성에 따라 이동

---
+++ src/plugIns/directive/index.js
@@ -0,0 +1,9 @@
+/**@type {(Vue:import("vue").VueConstructor)=>void} */
+export default function install(Vue) {
+  Vue.directive('touch', {
+    /**@type {import("vue").DirectiveFunction} */
+    bind(el, binding) {
+      
+    },
+  })
+}
